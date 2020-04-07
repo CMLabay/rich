22290807d9ff7121daa6ffdb2e6673bca9500eb3
Christina Labay <Christina.m.labay@gmail.com>
2020-04-07 11:33:09 -0500

    firrsrt

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Christina Labay on 4/6/20.
+//  Copyright © 2020 Christina Labay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
